4cc2d41475d287143298dc4b88844fecb916ff41
Bart Janssens <bart@bartjanssens.org>
2020-10-04 23:30:36 +0200

    Hack in CoolProp installation

--- src/MecaPluto.jl
+++ src/MecaPluto.jl
@@ -4,6 +4,9 @@ export runpluto
 
 import Pluto
 
+using Pkg
+pkg"add https://github.com/barche/CoolProp.jl.git#use-jll"
+
 function __init__()
   ENV["PLUTO_DEFAULT_ENVIRONMENT_PATH"] = dirname(@__DIR__)
 end
